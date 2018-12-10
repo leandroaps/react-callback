0d1462420e5e8fbaa20a575a6455e419e6f78d55
Leandro A. Siqueira <leandroaps@gmail.com>
2018-12-10 15:45:16 -0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Assets/Styles/starter-template.css";
+
+import React, { Component } from "react";
+
+import Main from "./Components/Main/";
+import Nav from "./Components/Nav";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <Nav />
+        <Main />
+      </React.Fragment>
     );
   }
 }
